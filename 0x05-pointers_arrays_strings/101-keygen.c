53d08f78d34c04cb6ce6dd009a72db4325ee31c9
BobbyRhodemi <akin.rhodes02@gmail.com>
2022-06-14 16:20:17 -0400

    101 keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Entry point
+*Return: return 0
+*/
+
+int main(void)
+{
+	int tmp;
+	int sum;
+	int val;
+
+	srand(time(NULL));
+	sum = 2772;
+	val = 0;
+
+	while (sum > 122)
+	{
+		tmp = (rand() % 100);
+		printf("%c", tmp);
+		sum -= tmp;
+		val++;
+	}
+	printf("%c", sum);
+        return 0;
+}
