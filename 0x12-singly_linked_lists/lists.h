d72e40115c9561bae58bec696935078abeae495d
BobbyRhodemi <akin.rhodes02@gmail.com>
2022-07-15 10:58:00 +0800

    fix betty style

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include "lists.h"
 
 /**
  * struct list_s - singly linked list
@@ -21,3 +22,5 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+
+#endif
